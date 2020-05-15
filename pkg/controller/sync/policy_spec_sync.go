2f070ee1ceed97805db094cfad7748c9d85eb6eb
Yu Cao <ycao@redhat.com>
2020-05-15 15:35:09 -0400

    generate events through lifecycle (#5)

--- pkg/controller/sync/policy_spec_sync.go
+++ pkg/controller/sync/policy_spec_sync.go
@@ -3,13 +3,19 @@ package sync
 
 import (
 	"context"
+	"fmt"
 
 	policiesv1 "github.com/open-cluster-management/governance-policy-propagator/pkg/apis/policies/v1"
 	"github.com/open-cluster-management/governance-policy-propagator/pkg/controller/common"
+	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
@@ -19,7 +25,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
-var log = logf.Log.WithName("policy-spec-sync")
+const controllerName string = "policy-spec-sync"
+
+var log = logf.Log.WithName(controllerName)
 
 // Add creates a new Policy Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
@@ -29,18 +37,35 @@ func Add(mgr manager.Manager, managedCfg *rest.Config) error {
 		log.Error(err, "Failed to generate client to managed cluster")
 		return err
 	}
-	return add(mgr, newReconciler(mgr, managedClient))
+	var kubeClient kubernetes.Interface = kubernetes.NewForConfigOrDie(managedCfg)
+	eventsScheme := runtime.NewScheme()
+	if err = v1.AddToScheme(eventsScheme); err != nil {
+		return err
+	}
+
+	eventBroadcaster := record.NewBroadcaster()
+	namespace, err := k8sutil.GetWatchNamespace()
+	if err != nil {
+		log.Error(err, "Failed to get watch namespace")
+		return err
+	}
+	eventBroadcaster.StartRecordingToSink(&corev1.EventSinkImpl{Interface: kubeClient.CoreV1().Events(namespace)})
+	managedRecorder := eventBroadcaster.NewRecorder(eventsScheme, v1.EventSource{Component: controllerName})
+
+	return add(mgr, newReconciler(mgr, managedClient, managedRecorder))
 }
 
 // newReconciler returns a new reconcile.Reconciler
-func newReconciler(mgr manager.Manager, managedClient client.Client) reconcile.Reconciler {
-	return &ReconcilePolicy{hubClient: mgr.GetClient(), managedClient: managedClient, scheme: mgr.GetScheme()}
+func newReconciler(mgr manager.Manager, managedClient client.Client,
+	managedRecorder record.EventRecorder) reconcile.Reconciler {
+	return &ReconcilePolicy{hubClient: mgr.GetClient(), managedClient: managedClient,
+		managedRecorder: managedRecorder, scheme: mgr.GetScheme()}
 }
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	// Create a new controller
-	c, err := controller.New("policy-spec-sync", mgr, controller.Options{Reconciler: r})
+	c, err := controller.New(controllerName, mgr, controller.Options{Reconciler: r})
 	if err != nil {
 		return err
 	}
@@ -61,9 +86,10 @@ var _ reconcile.Reconciler = &ReconcilePolicy{}
 type ReconcilePolicy struct {
 	// This client, initialized using mgr.Client() above, is a split client
 	// that reads objects from the cache and writes to the apiserver
-	hubClient     client.Client
-	managedClient client.Client
-	scheme        *runtime.Scheme
+	hubClient       client.Client
+	managedClient   client.Client
+	managedRecorder record.EventRecorder
+	scheme          *runtime.Scheme
 }
 
 // Reconcile reads that state of the cluster for a Policy object and makes changes based on the state read
@@ -108,15 +134,20 @@ func (r *ReconcilePolicy) Reconcile(request reconcile.Request) (reconcile.Result
 		if errors.IsNotFound(err) {
 			// not found on managed cluster, create it
 			reqLogger.Info("Policy not found on managed cluster, creating it...")
-			instance.SetOwnerReferences(nil)
-			instance.SetResourceVersion("")
-			err = r.managedClient.Create(context.TODO(), instance)
+			managedPlc = instance.DeepCopy()
+			managedPlc.SetOwnerReferences(nil)
+			managedPlc.SetResourceVersion("")
+			err = r.managedClient.Create(context.TODO(), managedPlc)
 			if err != nil {
 				reqLogger.Error(err, "Failed to create policy on managed...")
-				return reconcile.Result{}, r.managedClient.Create(context.TODO(), instance)
+				return reconcile.Result{}, err
 			}
+			r.managedRecorder.Event(instance, "Normal", "PolicySpecSync",
+				fmt.Sprintf("Policy %s was synchronized to cluster namespace %s", instance.GetName(),
+					instance.GetNamespace()))
 		}
 		reqLogger.Error(err, "Failed to get policy from managed...")
+		return reconcile.Result{}, err
 	}
 	// found, then compare and update
 	if !common.CompareSpecAndAnnotation(instance, managedPlc) {
@@ -129,6 +160,9 @@ func (r *ReconcilePolicy) Reconcile(request reconcile.Request) (reconcile.Result
 			reqLogger.Error(err, "Failed to update policy on managed...")
 			return reconcile.Result{}, err
 		}
+		r.managedRecorder.Event(instance, "Normal", "PolicySpecSync",
+			fmt.Sprintf("Policy %s was updated in cluster namespace %s", instance.GetName(),
+				instance.GetNamespace()))
 	}
 	reqLogger.Info("Reconciliation complete.")
 	return reconcile.Result{}, nil
