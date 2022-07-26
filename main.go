932ae62fc787de073b78ecec3120ef1378d961d3
Gus Parvin <gparvin@redhat.com>
2022-07-26 15:38:26 -0400

    Switch leader election default
    Current default is configmapsleases which is no longer preferred.
    Using leases instead.
    
    Refs:
     - https://github.com/stolostron/backlog/issues/19110
    
    Signed-off-by: Gus Parvin <gparvin@redhat.com>

--- main.go
+++ main.go
@@ -122,6 +122,10 @@ func main() {
 		// If legacyLeaderElection is enabled, then that means the lease API is not available.
 		// In this case, use the legacy leader election method of a ConfigMap.
 		options.LeaderElectionResourceLock = "configmaps"
+	} else {
+		// use the leases leader election by default for controller-runtime 0.11 instead of
+		// the default of configmapsleases (leases is the new default in 0.12)
+		options.LeaderElectionResourceLock = "leases"
 	}
 
 	// Create a new manager to provide shared dependencies and start components
