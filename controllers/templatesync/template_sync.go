8f38a69e056e28d7a613ef12bc56e6c3485f5daa
JeffeyL <49671320+JeffeyL@users.noreply.github.com>
2023-08-24 10:35:07 -0400

    Fix bug where syntax error on policy incremented wrong metric (#79)
    Simple checks added to differentiate between system and user errors to reduce false positive system errors
    
    Ref: https://issues.redhat.com/browse/ACM-3160
    
    Signed-off-by: Jeffrey Luo <jeluo@redhat.com>

--- controllers/templatesync/template_sync.go
+++ controllers/templatesync/template_sync.go
@@ -564,7 +564,12 @@ func (r *PolicyReconciler) Reconcile(ctx context.Context, request reconcile.Requ
 
 					tLogger.Error(resultError, "Failed to create policy template")
 
-					policySystemErrorsCounter.WithLabelValues(instance.Name, tName, "create-error").Inc()
+					// check for syntax error in policy
+					if k8serrors.IsInvalid(err) {
+						policyUserErrorsCounter.WithLabelValues(instance.Name, tName, "format-error").Inc()
+					} else {
+						policySystemErrorsCounter.WithLabelValues(instance.Name, tName, "create-error").Inc()
+					}
 
 					continue
 				}
@@ -757,7 +762,12 @@ func (r *PolicyReconciler) Reconcile(ctx context.Context, request reconcile.Requ
 
 				tLogger.Error(err, "Failed to update the policy template")
 
-				policySystemErrorsCounter.WithLabelValues(instance.Name, tName, "patch-error").Inc()
+				// check for syntax error in policy
+				if k8serrors.IsInvalid(err) {
+					policyUserErrorsCounter.WithLabelValues(instance.Name, tName, "format-error").Inc()
+				} else {
+					policySystemErrorsCounter.WithLabelValues(instance.Name, tName, "patch-error").Inc()
+				}
 
 				continue
 			}
